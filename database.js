922a75e20d3aa3008e647ca3747d0430090179a0
Soufiane <124498559+Soufi-ane@users.noreply.github.com>
2024-04-16 23:31:12 +0100

    rollback  database.js

--- database.js
+++ database.js
@@ -26,15 +26,12 @@ export async function findUser(ID) {
     return user;
 }
 
-export async function getUserLinks(ID) {
+export async function getUserInfo(ID) {
     const [links] = await pool.query("SELECT url , bg_color  , radius FROM users JOIN links ON links.user_id = users.id WHERE users.id = ?  ;", [ID]);
-    
-    return links ;
-}
-export async function getUserPageData(ID) {
     const [[pageData]] = await pool.query("SELECT name , username, background , font ,bio FROM users JOIN pages ON pages.user_id = users.id WHERE users.id = ?", [ID]);
-    return pageData ;
+    return [pageData , links] ;
 }
+
 
 export async function getLinkTree(username) {
     const [links] = await pool.query("SELECT url , bg_color  , radius FROM users JOIN links ON links.user_id = users.id WHERE username = ?  ;", [username]);
